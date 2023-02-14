65e4cf773182e0b4880a6326cd5bc970cd58eb08
Shubham Pathariya <77744790+spatharia276@users.noreply.github.com>
2023-02-14 15:43:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+import ImageSlider from './components/ImageSlider';
+import { SliderData } from './components/SliderData';
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <ImageSlider slides={SliderData} />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
